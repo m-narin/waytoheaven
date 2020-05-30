1483b3bff8c6be2955d4b8a758b09f78c3b5539f
mando <y2pgftpe@icloud.com>
2020-05-30 23:41:47 +0900

    contentnull禁止に修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,5 @@ class Tweet < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
     validates :deletekey, length: { is: 4}, presence: true
+    validates :content, presence: true
 end
